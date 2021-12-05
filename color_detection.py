dc3ad57b5db8a7bb5611a7c3048e3472c393e563
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2021-12-05 17:09:03 +0530

    Color Detection

--- color_detection.py
+++ color_detection.py
@@ -20,6 +20,7 @@ r = g = b = xpos = ypos = 0
 index = ["color", "color_name", "hex", "R", "G", "B"]
 csv = pd.read_csv('colors.csv', names=index, header=None)
 
+
 # function to calculate minimum distance from all colors and get the most matching color
 
 
@@ -31,6 +32,7 @@ def getcolorname(R, G, B):
             minimum = d
             cname = csv.loc[i, "color_name"]
     return cname
+
 
 # function to get x,y coordinates of mouse double click
 
@@ -45,8 +47,8 @@ def draw_function(event, x, y, flags, param):
         b = int(b)
         g = int(g)
         r = int(r)
-        
-       
+
+
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', draw_function)
 
@@ -54,24 +56,25 @@ while 1:
 
     cv2.imshow("image", img)
     if clicked:
-   
+
         # cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle
         cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
 
         # Creating text string to display( Color name and RGB values )
-        text = getcolorname(r, g, b) + ' R=' + str(r) + ' G='+str(g) + ' B='+str(b)
-        
+        text = getcolorname(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+
         # cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
         cv2.putText(img, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
 
         # For very light colours we will display text in black colour
-        if r+g+b >= 600:
+        if r + g + b >= 600:
             cv2.putText(img, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
-            
+
         clicked = False
 
     # Break the loop when user hits 'esc' key
     if cv2.waitKey(20) & 0xFF == 27:
         break
-    
+
 cv2.destroyAllWindows()
+
