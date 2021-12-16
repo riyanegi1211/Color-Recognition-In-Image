2b826e8c9d304befdc0c80d7bbc346add5ef1797
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2021-12-16 16:40:52 +0530

    Color Detection

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import cv2
+import pyaudio
+import platform
+import sys
+from bs4 import BeautifulSoup
+from gtts import gTTS
+import pyglet
+import os
+import time
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
@@ -14,22 +22,32 @@ while True:
 
     pixel_center = frame[cy, cx]
     hue_value = pixel_center[0]
+    sat_value = pixel_center[1]
+    val_value = pixel_center[2]
 
     color = "Undefined"
-    if hue_value < 5:
+    if hue_value == 0:
+        color = "BLACK"
+    elif hue_value < 5 and hue_value > 0:
         color = "RED"
-    elif hue_value > 5 and hue_value <= 22:
+    elif hue_value > 5 and hue_value <= 17:
         color = "ORANGE"
-    elif hue_value > 22 and hue_value <= 33:
+    elif hue_value > 17 and hue_value <= 30:
         color = "YELLOW"
-    elif hue_value > 33 and hue_value <= 78:
+    elif hue_value > 30 and hue_value <= 45:
+        color = "LIME GREEN"
+    elif hue_value > 45 and hue_value < 77:
         color = "GREEN"
-    elif hue_value > 78 and hue_value < 131:
-        color = "BLUE"
-    elif hue_value > 131 and hue_value < 167:
+    elif hue_value > 77 and hue_value < 98:
+        color = "SKY BLUE"
+    elif hue_value > 98 and hue_value < 102:
+        color = "LIGHT BLUE"
+    elif hue_value > 102 and hue_value < 126:
+        color = "DARK BLUE"
+    elif hue_value > 126 and hue_value < 138:
         color = "VIOLET"
-    else:
-        color = "RED"
+    elif hue_value > 138 and hue_value < 170:
+        color = "PINK"
 
     pixel_center_bgr = frame[cy,cx]
     cv2.putText(frame, color, (10,50), 0, 1, (255, 0, 0), 2)
@@ -37,7 +55,7 @@ while True:
 
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1)
-    if key == 27:   #27 is for esc key
+    if key == 27:
         break
 
 cap.release()
