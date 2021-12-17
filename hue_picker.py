2f05553466522aee790837797babe0df5605e83c
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2021-12-17 15:39:27 +0530

    Color Detection

--- hue_picker.py
+++ hue_picker.py
@@ -7,26 +7,24 @@ def nothing(x):
 # Trackbar
 
 
-cv2.namedWindow("frame")
-cv2.createTrackbar("H", "frame", 0, 179, nothing)
-cv2.createTrackbar("S", "frame", 255, 255, nothing)
-cv2.createTrackbar("V", "frame", 255, 255, nothing)
- 
+cv2.namedWindow("Hue Picker")
+cv2.createTrackbar("H", "Hue Picker", 0, 255, nothing)
+cv2.createTrackbar("S", "Hue Picker", 255, 255, nothing)
+cv2.createTrackbar("V", "Hue Picker", 255, 255, nothing)
+
 img_hsv = np.zeros((250, 500, 3), np.uint8)
 
 while True:
-    h = cv2.getTrackbarPos("H", "frame")
-    s = cv2.getTrackbarPos("S", "frame")
-    v = cv2.getTrackbarPos("V", "frame")
+    h = cv2.getTrackbarPos("H", "Hue Picker")
+    s = cv2.getTrackbarPos("S", "Hue Picker")
+    v = cv2.getTrackbarPos("V", "Hue Picker")
 
     img_hsv[:] = (h, s, v)
     img_bgr = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR)
 
-    cv2.imshow("frame", img_bgr)
+    cv2.imshow("Hue Picker", img_bgr)
     key = cv2.waitKey(1)
     if key == 27:
         break
-
-
 cv2.destroyAllWindows()
 
