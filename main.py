8307d80bf5fa921a2ff0cba4aa909c2872818977
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2021-12-17 15:38:17 +0530

    Color Detection

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ while True:
     val_value = pixel_center[2]
 
     color = "Undefined"
-    if val_value >= 0 and val_value<= 50:
+    if (val_value == 0) and (sat_value <= 255) and (hue_value <=255):
         color = "BLACK"
         s = gTTS(text="Black", lang='en')
         tsname = "name.mp3"
@@ -33,7 +33,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif sat_value < 30 and val_value > 180:
+    elif (hue_value <= 255) and (sat_value <= 50) and (val_value >= 210 and val_value <= 255):
         color = "WHITE"
         s = gTTS(text="White", lang='en')
         tsname = "name.mp3"
@@ -44,7 +44,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif hue_value < 5 and hue_value > 0:
+    elif (hue_value <= 5) and (sat_value <= 100) and (val_value >= 180 and val_value <= 255):
         color = "RED"
         s = gTTS(text="Red", lang='en')
         tsname = "name.mp3"
@@ -55,7 +55,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif hue_value > 5 and hue_value <= 17:
+    elif (hue_value > 5 and hue_value <= 17) and (sat_value >=150 and sat_value <= 255) and (val_value >= 200 and val_value <= 255):
         color = "ORANGE"
         s = gTTS(text="Orange", lang='en')
         tsname = "name.mp3"
@@ -66,7 +66,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif hue_value > 17 and hue_value <= 30:
+    elif (hue_value > 17 and hue_value <= 37) and (sat_value >= 110 and sat_value <= 180) and (val_value >= 220 and val_value <= 255):
         color = "YELLOW"
         s = gTTS(text="Yellow", lang='en')
         tsname = "name.mp3"
@@ -77,7 +77,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif hue_value > 30 and hue_value <= 45:
+    elif (hue_value > 38 and hue_value <= 48) and (sat_value >= 140 and sat_value <= 190) and (val_value >= 230 and val_value <= 255):
         color = "LIME GREEN"
         s = gTTS(text="Lime Green", lang='en')
         tsname = "name.mp3"
@@ -88,7 +88,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif hue_value > 45 and hue_value < 77:
+    elif (hue_value > 48 and hue_value < 70) and (sat_value >= 225 and sat_value <= 255) and (val_value >= 220 and val_value <= 255) :
         color = "GREEN"
         s = gTTS(text="Green", lang='en')
         tsname = "name.mp3"
@@ -110,7 +110,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif hue_value > 98 and hue_value < 102:
+    elif (hue_value > 80 and hue_value < 100) and (sat_value >= 70 and sat_value <= 200) and (val_value >= 245 and val_value <= 255):
         color = "LIGHT BLUE"
         s = gTTS(text="Light Blue", lang='en')
         tsname = "name.mp3"
@@ -121,7 +121,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif hue_value > 102 and hue_value < 126:
+    elif (hue_value > 102 and hue_value < 126) and (sat_value >= 225 and sat_value <= 255) and (val_value >= 35 and val_value <= 255):
         color = "DARK BLUE"
         s = gTTS(text="Dark Blue", lang='en')
         tsname = "name.mp3"
@@ -132,7 +132,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif hue_value > 126 and hue_value < 138:
+    elif (hue_value > 140 and hue_value < 150) and (sat_value >= 80 and sat_value <= 255) and (val_value >= 150 and val_value >= 210):
         color = "VIOLET"
         s = gTTS(text="Violet", lang='en')
         tsname = "name.mp3"
@@ -143,7 +143,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif hue_value > 138 and hue_value < 170:
+    elif (hue_value > 155 and hue_value < 172) and (sat_value >= 100 and sat_value <= 255) and (val_value >= 220 and val_value <= 255):
         color = "PINK"
         s = gTTS(text="Pink", lang='en')
         tsname = "name.mp3"
@@ -155,7 +155,7 @@ while True:
         time.sleep(music.duration)
         os.remove(tsname)
 
-    pixel_center_bgr = frame[cy,cx]
+    pixel_center_bgr = frame[cy, cx]
     cv2.putText(frame, color, (10,50), 0, 1, (255, 0, 0), 2)
     cv2.circle(frame, (cx, cy), 5, (255, 0, 0), 3)
 
