4784b16543f4714374a10a2237d504fc4df027e2
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2021-12-17 14:08:07 +0530

    Color Detection

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import cv2
-import pyaudio
-import platform
-import sys
-from bs4 import BeautifulSoup
 from gtts import gTTS
 import pyglet
 import os
@@ -26,28 +22,138 @@ while True:
     val_value = pixel_center[2]
 
     color = "Undefined"
-    if hue_value == 0:
+    if val_value >= 0 and val_value<= 50:
         color = "BLACK"
+        s = gTTS(text="Black", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
+    elif sat_value < 30 and val_value > 180:
+        color = "WHITE"
+        s = gTTS(text="White", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
     elif hue_value < 5 and hue_value > 0:
         color = "RED"
+        s = gTTS(text="Red", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
     elif hue_value > 5 and hue_value <= 17:
         color = "ORANGE"
+        s = gTTS(text="Orange", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
     elif hue_value > 17 and hue_value <= 30:
         color = "YELLOW"
+        s = gTTS(text="Yellow", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
     elif hue_value > 30 and hue_value <= 45:
         color = "LIME GREEN"
+        s = gTTS(text="Lime Green", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
     elif hue_value > 45 and hue_value < 77:
         color = "GREEN"
+        s = gTTS(text="Green", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
     elif hue_value > 77 and hue_value < 98:
         color = "SKY BLUE"
+        s = gTTS(text="Sky Blue", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
     elif hue_value > 98 and hue_value < 102:
         color = "LIGHT BLUE"
+        s = gTTS(text="Light Blue", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
     elif hue_value > 102 and hue_value < 126:
         color = "DARK BLUE"
+        s = gTTS(text="Dark Blue", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
     elif hue_value > 126 and hue_value < 138:
         color = "VIOLET"
+        s = gTTS(text="Violet", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
     elif hue_value > 138 and hue_value < 170:
         color = "PINK"
+        s = gTTS(text="Pink", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
 
     pixel_center_bgr = frame[cy,cx]
     cv2.putText(frame, color, (10,50), 0, 1, (255, 0, 0), 2)
