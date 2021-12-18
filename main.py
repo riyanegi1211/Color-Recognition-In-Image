9554ca0799538d78d313b840dd6d0c0c719a1be4
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2021-12-18 17:00:57 +0530

    Color Detection

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
+# To take a series of frame
 while True:
     _, frame = cap.read()
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     height, width, _ = frame.shape
 
+# To find the center of the frame
     cx = int(width/2)
     cy = int(height/2)
 
     pixel_center = frame[cy, cx]
+    print(pixel_center)
     hue_value = pixel_center[0]
     sat_value = pixel_center[1]
     val_value = pixel_center[2]
@@ -33,7 +36,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif (val_value == 255):
+    elif (hue_value >= 240 and hue_value <= 255) and (sat_value >= 242 and sat_value<= 255) and (val_value >= 225 and val_value <= 255):
         color = "WHITE"
         s = gTTS(text="White", lang='en')
         tsname = "name.mp3"
@@ -44,7 +47,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif (hue_value <= 5) :
+    elif (hue_value <= 18) :
         color = "RED"
         s = gTTS(text="Red", lang='en')
         tsname = "name.mp3"
@@ -55,7 +58,7 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
-    elif (hue_value >= 6 and hue_value <= 16) :
+    elif (hue_value >= 80 and hue_value <= 90) and (sat_value >= 205 and sat_value <= 235) and (val_value ==255) :
         color = "ORANGE"
         s = gTTS(text="Orange", lang='en')
         tsname = "name.mp3"
@@ -143,6 +146,18 @@ while True:
 
         time.sleep(music.duration)
         os.remove(tsname)
+    else:
+        color = "RED"
+        s = gTTS(text="Red", lang='en')
+        tsname = "name.mp3"
+        s.save(tsname)
+
+        music = pyglet.media.load(tsname, streaming=False)
+        music.play()
+
+        time.sleep(music.duration)
+        os.remove(tsname)
+
 
     pixel_center_bgr = frame[cy, cx]
     cv2.putText(frame, color, (10,50), 0, 1, (255, 0, 0), 2)
